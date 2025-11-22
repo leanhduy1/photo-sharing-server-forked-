06d8dc9a272452de0aa64967f71336426f7340f1
leanhduy1 <duy200496@gmail.com>
2025-11-22 16:01:47 +0700

    xong TH3

--- routes/PhotoRouter.js
+++ routes/PhotoRouter.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const Photo = require("../db/photoModel");
+const User = require("../db/userModel");
 const router = express.Router();
 
-router.post("/", async (request, response) => {
-  
-});
+router.get("/photosOfUser/:id", async (request, response) => {
+	const id = request.params.id;
+	try {
+		const photos = await Photo.find({ user_id: id }, "_id file_name date_time user_id comments");
 
-router.get("/", async (request, response) => {
-  
-});
+		if (!photos) {
+			return response.status(400).json({ message: "Photos not found" });
+		}
+
+		const photosList = JSON.parse(JSON.stringify(photos));
+		for (const photo of photosList) {
+			if (photo.comments && photo.comments.length > 0) {
+				for (const comment of photo.comments) {
+					const user = await User.findById(comment.user_id, "_id first_name last_name");
+					comment.user = user;
+					delete comment.user_id;
+				}
+			}
+		}
+
+		response.json(photosList);
+	} catch (err) {
+		response.status(400).json({ message: "Invalid user id", error: err });
+	}
+})
 
 module.exports = router;
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -2,12 +2,27 @@ const express = require("express");
 const User = require("../db/userModel");
 const router = express.Router();
 
-router.post("/", async (request, response) => {
-  
+router.get("/list", async (request, response) => {
+  try {
+		const users = await User.find({}, "_id first_name last_name");
+		response.json(users);
+	} catch (err) {
+		response.status(500).json({ error: err });
+	}
 });
 
-router.get("/", async (request, response) => {
-  
-});
+router.get("/:id", async (request, response) => {
+	const id = request.params.id;
+	try {
+		const user = await User.findById(id, "_id first_name last_name location description occupation");
+		if (!user) {
+			return response.status(400).json({ message: "User not found" });
+		}
+		response.json(user);
+	} catch (err) {
+		response.status(400).json({ message: "Invalid user id", error: err });
+	}
+})
+
 
 module.exports = router;
